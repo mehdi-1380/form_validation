53c7e88fb27cbf9aec53814dd7b893847977ad72
Mehdi <mehdivojudi00@gmail.com>
2026-06-29 00:49:50 -0700

    new

--- js/form.js
+++ js/form.js
@@ -1,3 +1,5 @@
+let boolian = false;
+
 function validation(event){
 
     let input = event.target;
@@ -22,6 +24,7 @@ function validation(event){
     }
 
 }
+}
 
 function emailcheck(span,input){
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
@@ -40,19 +43,23 @@ function empty(span,input){
 
     if(input.value == ''){
         invalid(span,"لطفا فیلد را پر کنید");
+        
     }else if(input.value != ''){
         valid(span);
+        
     }
 }
-}
+
 
 function valid(span){
     span.innerHTML = '' ;
+    
 }
 
 function invalid(span,msg){
     span.innerHTML = msg;
-    span.style.color = "red";
+    span.className = "error";
+    boolian = true;
 }
 
 
@@ -65,7 +72,27 @@ function invalid(span,msg){
 function number(span,input){
     if(isNaN(input.value)){
         invalid(span,"لطفا عدد وارد کنید");
-    }else if(span.value != '' && !isNaN(span.value)){
+    }else if(input.value != '' && !isNaN(input.value)){
         valid(span);
+    }
+}
+
+
+function checkAll(event){
+
+    boolian = false ;
+    
+    let inputs = document.querySelectorAll('input');
+
+
+    for(let item of inputs){
+        let parent = item.parentElement;
+        let span = parent.lastElementChild;
+        if(item.value == ''){
+            invalid(span,"لطفا فیلد را پر کنید");
+        }
+    }
+    if(boolian){
+        event.preventDefault();
     }
 }
